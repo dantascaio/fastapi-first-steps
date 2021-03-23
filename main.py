5c6b3e19ad02e7e9de16c47326f9b9f863303e1c
Caio Cesar S Dantas <caiodantas@Caios-MacBook-Pro.local>
2021-03-22 23:20:26 -0300

    query parameters finished

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
+from typing import Optional
+
 from fastapi import FastAPI
 from models import user
+from models.model import ModelName
+
 app = FastAPI()
 
 
 @app.get("/consulta-chave/{chave}")
-async def root(chave):
+async def root(chave: str):
     return {"message": f"Sua chave é: {chave}"}
 
 
 @app.get("/consulta-bool/{bool}")
-async def root(entrada: bool):
+async def boolean(entrada: bool):
     return {"A entrada é verdadeira"} if entrada else {"A entrada não é verdadeira"}
 
+@app.get("/models/{model_name}")
+async def select_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return { "model_name" : model_name, "message": "Alexnet rocks!!" }
+    if model_name.value == "resnet":
+        return { "model_name" : model_name, "message": "Let's use some resnet!" }
+    return {"model_name" : model_name, "message": "I'm not sure about this one"}
 
-@app.get("/detalha-usuario/{user_received}")
-async def root(user_received: user.User):
-    user_teste = user.User("Caio Dantas", "F1704955")
-    return user_teste.to_json()
+@app.get("/select-item/{user_id}/items/{item_id}")
+async def select_user_item(user_id : int, item_id: int, details: Optional[str] = "no details",
+                           full_context: bool = False):
+    item = {"Item_id": item_id, "Owner_id": user_id}
+    if details != 'no details':
+        item.update({"Details" : details})
+    if full_context:
+        item.update({"Context": f"The item {item_id} belongs to User {user_id}"})
+    return item
+
+
+
+
+
+
+# @app.get("/detalha-usuario/{user_received}")
+# async def root(user_received: user.User):
+#     user_teste = user.User("Caio Dantas", "F1704955")
+#     return user_teste.to_json()
 
